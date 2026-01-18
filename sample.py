a5cf569ef67be14eedfc1dc3899dbf1138011041
yasu <yasu4155@gmail.com>
2026-01-18 17:49:57 +0900

    14th commit

--- sample.py
+++ sample.py
@@ -1,8 +1,3 @@
 import streamlit as st
-import cv2
-import mediapipe as mp
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_holistic = mp.solutions.holistic
-# Webカメラから入力
+
 print('test')
